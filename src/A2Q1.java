64016439e3e762e24603393de267aaebb9639f19
micla1676 <micla1676@HRH-IC0027336.wrdsb.ca>
2016-09-21 10:06:35 -0400

    Added A2Q1 Added A2Q2 Added A2Q3

--- src/A2Q1.java
+++ src/A2Q1.java
@@ -5,10 +5,8 @@ import becker.robots.Robot;
 import becker.robots.Thing;
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ Get karel to pick up 7 things then move over he last 3 using loops
  */
-
 /**
  *
  * @author micla1676
@@ -21,43 +19,38 @@ public class A2Q1 {
     public static void main(String[] args) {
         //create city
         City Thingcity = new City();
-        
+
         //create robot
-        
-        Robot karel = new Robot (Thingcity,1,1,Direction.EAST);
-        
+
+        Robot karel = new Robot(Thingcity, 1, 1, Direction.EAST);
+
         //create things
-        
-        new Thing (Thingcity,1,2);
-        new Thing (Thingcity,1,3);
-        new Thing (Thingcity,1,4);
-        new Thing (Thingcity,1,5);
-        new Thing (Thingcity,1,6);
-        new Thing (Thingcity,1,7);
-        new Thing (Thingcity,1,8);
-        new Thing (Thingcity,1,9);
-        new Thing (Thingcity,1,10);
-        new Thing (Thingcity,1,11);
-        
-        //create integer
-        
-        int moves = 0;
-        
-        while (moves < 7){
+
+        new Thing(Thingcity, 1, 2);
+        new Thing(Thingcity, 1, 3);
+        new Thing(Thingcity, 1, 4);
+        new Thing(Thingcity, 1, 5);
+        new Thing(Thingcity, 1, 6);
+        new Thing(Thingcity, 1, 7);
+        new Thing(Thingcity, 1, 8);
+        new Thing(Thingcity, 1, 9);
+        new Thing(Thingcity, 1, 10);
+        new Thing(Thingcity, 1, 11);
+
+        //allow movement while moves < 7
+        while (karel.countThingsInBackpack() < 7) {
             karel.move();
             karel.pickThing();
-            moves = moves + 1;
         }
-        
+        //move 1 space
         karel.move();
-        
-        
-        
-        if(karel.canPickThing()){
+
+        //move while Karel can pick up a "thing"
+        while (karel.canPickThing()) {
             karel.move();
-            goto
+
         }
-            
-        
+
+
     }
 }
