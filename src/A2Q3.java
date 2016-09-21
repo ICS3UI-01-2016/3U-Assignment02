d91a728a8621f238afa837b5b3b1fd65671fcca3
micla1676 <micla1676@HRH-IC0027336.wrdsb.ca>
2016-09-21 10:14:18 -0400

    changed A2Q3

--- src/A2Q3.java
+++ src/A2Q3.java
@@ -1,18 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/*
+ * 21/09/2016
+ * Write an algorithm that gets Karel back to 0,0 no matter
+   where he is in city
  * @author micla1676
  */
+
 public class A2Q3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create city for robot
+        City Algorithm = new City ();
+        
+        //create Robot
+        
+        Robot Algorbot = new Robot (Algorithm,9,9,Direction.SOUTH);
+        
+        //algorithm to get robot back to 0,0
+        
+        
+        
+        
+        
+        
+        
     }
 }
