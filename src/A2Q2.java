40005c193befcec3f5ed0cc212b14873d5c33e23
baayl <baayl@192.168.0.12>
2016-09-26 16:49:14 -0400

    Jumps an imaginary Hurdle. FeelsGoodMan

--- src/A2Q2.java
+++ src/A2Q2.java
@@ -22,12 +22,9 @@ public class A2Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // create a city
         City af = new City();
-        // create a robot
-        Robot Karel = new Robot(af,1,1,Direction.EAST);
+        Robot Rem = new Robot(af,1,1,Direction.EAST);
         
-        new Thing(af,1,9);
         // create hurdles
         new Wall(af,1,1,Direction.SOUTH);
         new Wall(af,1,2,Direction.SOUTH);
@@ -38,94 +35,40 @@ public class A2Q2 {
         new Wall(af,1,7,Direction.SOUTH);
         new Wall(af,1,8,Direction.SOUTH);
         new Wall(af,1,9,Direction.SOUTH);
+        new Wall(af,1,7,Direction.EAST);
+        new Wall(af,1,1,Direction.EAST);
         new Wall(af,1,2,Direction.EAST);
-        new Wall(af,1,3,Direction.EAST);
-        new Wall(af,1,8,Direction.EAST);
+        new Wall(af,1,4,Direction.EAST);
+        new Thing(af,1,9);
+       
         
-        // make karel jump hurdles
-        
-        
-        while(Karel.frontIsClear()){
-             Karel.move();
+        while(true){
+            // make Rem jump hurdles
+            if(!Rem.frontIsClear()){
+                Rem.turnLeft();
+                
+                
+            }else{
+                Rem.move();
+            
+        // make Rem turn right!
+        if(Rem.frontIsClear()){
+            Rem.turnLeft();
+            Rem.turnLeft();
+            Rem.turnLeft();
+        }
+        // make Rem stop near Thing
+        if(Rem.getAvenue()==9){
              
+                
+                 if(Rem.getStreet()==1)
+                 Rem.pickThing();
+                
+             break;
         }
         
-        if(!Karel.frontIsClear()){
-            Karel.turnLeft();
-            Karel.move();
-            Karel.turnLeft();
-            Karel.turnLeft();
-            Karel.turnLeft();
-        
-            {
-            
-        Karel.move();
-        Karel.turnLeft();
-        Karel.turnLeft();
-        Karel.turnLeft();
-        Karel.move();
-        Karel.turnLeft();
-           
-        }
-        while(Karel.frontIsClear()){
-            Karel.move();
-        }
-        if(!Karel.frontIsClear()){
-            Karel.turnLeft();
-            Karel.move();
-            Karel.turnLeft();
-            Karel.turnLeft();
-            Karel.turnLeft();
-            
-            {    
-            
-        Karel.move();
-        Karel.turnLeft();
-        Karel.turnLeft();
-        Karel.turnLeft();
-        Karel.move();
-        Karel.turnLeft();
     }
-        while(Karel.frontIsClear()){
-            Karel.move();
-    }
-        if(!Karel.frontIsClear()){
-            Karel.turnLeft();
-            Karel.move();
-            Karel.turnLeft();
-            Karel.turnLeft();
-            Karel.turnLeft();
-}
-        Karel.move();
-        Karel.turnLeft();
-        Karel.turnLeft();
-        Karel.turnLeft();
-        Karel.move();
-        Karel.turnLeft();
-        
-}
-        if(Karel.canPickThing()){
-            Karel.pickThing();
-        }
-        while(Karel.countThingsInBackpack() == 0){
-            Karel.move();
-            
-          
-         
-        
-        if(Karel.canPickThing()){
-            Karel.pickThing();
-        }
-        else; {
-        
-           
- 
-     
-        
-}
-}
-}
     }
 }
-
+}
 
