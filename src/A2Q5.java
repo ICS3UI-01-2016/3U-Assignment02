016aa2f586212e8fb9c1e6de465f2dbe792c265b
micla1676 <micla1676@HRH-IC0027336.wrdsb.ca>
2016-09-27 10:07:12 -0400

    started A2Q5

--- src/A2Q5.java
+++ src/A2Q5.java
@@ -1,10 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import java.awt.Color;
+
+/*27/09/2016
+ *A2Q5
+ *Clean a sidewalk
  * @author micla1676
  */
 public class A2Q5 {
@@ -13,6 +17,146 @@ public class A2Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create city
+        City Street = new City();
+        
+        //create shovelbots
+        Robot karel = new Robot(Street,1,3,Direction.SOUTH);
+        Robot tina = new Robot(Street,1,3,Direction.SOUTH);
+        
+        
+        //lable karel
+        karel.setLabel("K");
+        //set color of karel
+        karel.setColor(Color.BLUE);
+        // set transparency of karel 
+        karel.setTransparency(0.5);
+        
+        //lable tina
+        tina.setLabel("T");
+                //set color of Tina
+        tina.setColor(Color.RED);
+        // set transparency of Tina
+        tina.setTransparency(0.5);
+        
+        
+        //create road of street
+        new Wall (Street,1,1,Direction.WEST);
+        new Wall (Street,2,1,Direction.WEST);
+        new Wall (Street,3,1,Direction.WEST);
+        new Wall (Street,4,1,Direction.WEST);
+        new Wall (Street,5,1,Direction.WEST);
+        new Wall (Street,6,1,Direction.WEST);
+        new Wall (Street,7,1,Direction.WEST);
+        new Wall (Street,8,1,Direction.WEST);
+        new Wall (Street,9,1,Direction.WEST);
+        new Wall (Street,10,1,Direction.WEST);
+        
+        new Wall (Street,1,2,Direction.EAST);
+        new Wall (Street,2,2,Direction.EAST);
+        new Wall (Street,3,2,Direction.EAST);
+        new Wall (Street,4,2,Direction.EAST);
+        new Wall (Street,5,2,Direction.EAST);
+        new Wall (Street,6,2,Direction.EAST);
+        new Wall (Street,7,2,Direction.EAST);
+        new Wall (Street,8,2,Direction.EAST);
+        new Wall (Street,9,2,Direction.EAST);
+        new Wall (Street,10,2,Direction.EAST);
+        new Wall (Street,1,3,Direction.EAST);
+        new Wall (Street,4,3,Direction.EAST);
+        new Wall (Street,6,3,Direction.EAST);
+        new Wall (Street,7,3,Direction.EAST);
+        new Wall (Street,10,3,Direction.EAST);
+        new Wall (Street,10,3,Direction.SOUTH);
+        
+        new Wall (Street,2,4,Direction.NORTH);
+        new Wall (Street,2,5,Direction.NORTH);
+        new Wall (Street,2,6,Direction.NORTH);
+        new Wall (Street,2,7,Direction.NORTH);
+        new Wall (Street,2,7,Direction.EAST);
+        new Wall (Street,3,7,Direction.EAST);
+        new Wall (Street,3,6,Direction.SOUTH);
+        new Wall (Street,3,5,Direction.SOUTH);
+        new Wall (Street,3,4,Direction.SOUTH);
+        new Wall (Street,3,7,Direction.SOUTH);
+        
+        new Wall (Street,8,4,Direction.NORTH);
+        new Wall (Street,8,5,Direction.NORTH);
+        new Wall (Street,8,6,Direction.NORTH);
+        new Wall (Street,8,7,Direction.NORTH);
+        new Wall (Street,8,8,Direction.NORTH);
+        new Wall (Street,8,8,Direction.EAST);
+        new Wall (Street,9,8,Direction.EAST);
+        new Wall (Street,9,6,Direction.SOUTH);
+        new Wall (Street,9,5,Direction.SOUTH);
+        new Wall (Street,9,4,Direction.SOUTH);
+        new Wall (Street,9,7,Direction.SOUTH);
+        new Wall (Street,9,8,Direction.SOUTH);
+        
+        new Wall (Street,5,4,Direction.NORTH);
+        new Wall (Street,5,4,Direction.SOUTH);
+        new Wall (Street,5,5,Direction.NORTH);
+        new Wall (Street,5,5,Direction.SOUTH);
+        new Wall (Street,5,6,Direction.NORTH);
+        new Wall (Street,5,6,Direction.SOUTH);
+        new Wall (Street,5,6,Direction.EAST);
+        
+        new Wall (Street,1,3,Direction.NORTH);
+        
+        
+        //create things
+        new Thing(Street,2,3);
+        new Thing(Street,2,4);
+        new Thing(Street,2,5);
+        new Thing(Street,2,6);
+        new Thing(Street,3,3);
+        new Thing(Street,3,5);
+        new Thing(Street,3,7);
+        new Thing(Street,5,3);
+        new Thing(Street,5,4);
+        new Thing(Street,5,5);
+        new Thing(Street,8,3);
+        new Thing(Street,8,4);
+        new Thing(Street,8,8);
+        new Thing(Street,9,4);
+        new Thing(Street,9,5);
+        new Thing(Street,9,7);
+        
+        
+        
+        //create algorithm to shovel things
+        
+        while(true){
+            
+            
+            
+             if(karel.canPickThing()){
+                 karel.pickThing();
+                 karel.turnLeft();
+                 karel.move();
+                 if(karel.frontIsClear()){
+                     karel.move();
+                 } else {karel.turnLeft();}
+            
+                
+            }else if(karel.frontIsClear()){
+                     karel.move();
+                 } else {karel.turnLeft();}
+            
+            
+        }
+        
+        
+        
+                
+                
+                
+                
+                
+                
+                
+                
+        
+        
     }
 }
