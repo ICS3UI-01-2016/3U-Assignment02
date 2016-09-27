4dc0daacb5405337b2e2d0d44511ce1b514a4961
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2016-09-27 10:14:18 -0400

    update on assign.2

--- src/a2q5.java
+++ src/a2q5.java
@@ -23,7 +23,7 @@ public class a2q5 {
         //create a new city
             City mtl = new City();
             //create a robot
-            Robot candice = new Robot (mtl,1,1,Direction.EAST);
+            Robot candice = new Robot (mtl,0,2,Direction.SOUTH);
             //change color of robot
             candice.setColor(Color.black);
             //build driveway,sidewalk,etc.
@@ -33,10 +33,21 @@ public class a2q5 {
             new Wall(mtl,3,0,Direction.WEST);
             new Wall(mtl,4,0,Direction.WEST);
             new Wall(mtl,5,0,Direction.WEST);
-            new Wall(mtl,6,0,Direction.SOUTH);
-            new Wall(mtl,7,0,Direction.SOUTH);
-            new Wall(mtl,8,0,Direction.SOUTH);
+            new Wall(mtl,6,0,Direction.WEST);
+            new Wall(mtl,7,0,Direction.WEST);
+            new Wall(mtl,8,0,Direction.WEST);
             //make ten walls going down avenue,starting east 2+ walls of previous
             //turns left sees driveway... cleans it out by going down and back dowwn and back
+            new Wall(mtl,0,1,Direction.EAST);
+            new Wall(mtl,1,1,Direction.EAST);
+            new Wall(mtl,2,1,Direction.EAST);
+            new Wall(mtl,3,1,Direction.EAST);
+            new Wall(mtl,4,1,Direction.EAST);
+            new Wall(mtl,5,1,Direction.EAST);
+            new Wall(mtl,6,1,Direction.EAST);
+            new Wall(mtl,7,1,Direction.EAST);
+            new Wall(mtl,8,1,Direction.EAST);
+            //moreee
+            new Wall(mtl,0,2,Direction.EAST);
     }
 }
