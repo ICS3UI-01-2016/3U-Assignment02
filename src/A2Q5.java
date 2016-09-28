5d0cbf59caf15b7726fd83ad1094582ec43a7290
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2016-09-28 10:14:34 -0400

    finish it

---
+++ src/A2Q5.java
@@ -0,0 +1,83 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fabed2976
+ */
+public class A2Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         City PH = new City ();
+         Robot Kris = new Robot (PH, 0,3, Direction.SOUTH);
+         
+         new Wall (PH, 1,2, Direction. EAST);
+         new Wall (PH, 2,2, Direction. EAST);
+         new Wall (PH, 3,2, Direction. EAST);
+         new Wall (PH, 4,2, Direction. EAST);
+         new Wall (PH, 5,2, Direction. EAST);
+         new Wall (PH, 6,2, Direction. EAST);
+         new Wall (PH, 7,2, Direction. EAST);
+         new Wall (PH, 8,2, Direction. EAST);
+         new Wall (PH, 9,2, Direction. EAST);
+         new Wall (PH, 10,2, Direction. EAST);
+         new Wall (PH, 1,1, Direction. WEST);
+         new Wall (PH, 2,1, Direction. WEST);
+         new Wall (PH, 3,1, Direction. WEST);
+         new Wall (PH, 4,1, Direction. WEST);
+         new Wall (PH, 5,1, Direction. WEST);
+         new Wall (PH, 6,1, Direction. WEST);
+         new Wall (PH, 7,1, Direction. WEST);
+         new Wall (PH, 8,1, Direction. WEST);
+         new Wall (PH, 9,1, Direction. WEST);
+         new Wall (PH, 10,1, Direction. WEST);
+         new Wall (PH, 1,3, Direction. EAST);
+         new Wall (PH, 2,4, Direction. NORTH);
+         new Wall (PH, 2,5, Direction. NORTH);
+         new Wall (PH, 2,6, Direction. NORTH);
+         new Wall (PH, 2,7, Direction. NORTH);
+         new Wall (PH, 2,7, Direction. EAST);
+         new Wall (PH,3,7, Direction. EAST);
+         new Wall (PH, 3,4, Direction. SOUTH);
+         new Wall (PH, 3,5, Direction. SOUTH);
+         new Wall (PH, 3,6, Direction. SOUTH);
+         new Wall (PH, 3,7, Direction. SOUTH);
+         new Wall (PH, 4,3, Direction. EAST);
+         new Wall (PH, 5,4, Direction. NORTH);
+         new Wall (PH, 5,5, Direction. NORTH);
+         new Wall (PH, 5,6, Direction. NORTH);
+         new Wall (PH, 5,6, Direction. EAST);
+         new Wall (PH, 5,4, Direction. SOUTH);
+         new Wall (PH, 5,5, Direction. SOUTH);
+         new Wall (PH, 5,6, Direction. SOUTH);
+         new Wall (PH, 6,3, Direction. EAST);
+         new Wall (PH, 7,3, Direction. EAST);
+         new Wall (PH, 8,4, Direction. NORTH);
+         new Wall (PH, 8,5, Direction. NORTH);
+         new Wall (PH, 8,6, Direction. NORTH);
+         new Wall (PH, 8,7, Direction. NORTH);
+         new Wall (PH, 8,7, Direction. EAST);
+         new Wall (PH, 9,7, Direction. EAST);
+         new Wall (PH, 9,4, Direction. SOUTH);
+         new Wall (PH, 9,5, Direction. SOUTH);
+         new Wall (PH, 9,6, Direction. SOUTH);
+         new Wall (PH, 9,7, Direction. SOUTH);
+         new Wall (PH, 10,3, Direction. EAST);
+         new Wall (PH, 2,1, Direction. WEST);
+         new Wall (PH, 2,1, Direction. WEST);
+                
+         
+    }
+}
