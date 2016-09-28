4d568fb3ee23b1ce0410cf93883f9cb143b52439
gavra1870 <gavra1870@HRH-IC0027342.wrdsb.ca>
2016-09-28 10:14:19 -0400

    Im done

---
+++ src/A2Q5.java
@@ -0,0 +1,91 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gavra1870
+ */
+public class A2Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Create a city for robot
+        City Ui = new City();
+        Robot Bob = new Robot (Ui, 1, 1, Direction.SOUTH);  
+        
+        //Create Walls
+        
+         new Wall (Ui, 1, 1,Direction.WEST);
+         new Wall (Ui, 2, 1,Direction.WEST);
+         new Wall (Ui, 3, 1,Direction.WEST);
+         new Wall (Ui, 4, 1,Direction.WEST);
+         new Wall (Ui, 5, 1,Direction.WEST);
+         new Wall (Ui, 6, 1,Direction.WEST);
+         new Wall (Ui, 7, 1,Direction.WEST);
+         new Wall (Ui, 8, 1,Direction.WEST);
+         new Wall (Ui, 9, 1,Direction.WEST);
+         new Wall (Ui, 10, 1,Direction.WEST);
+         new Wall (Ui, 10, 1,Direction.SOUTH);
+         new Wall (Ui, 10, 1,Direction.EAST);
+         new Wall (Ui, 9, 2,Direction.SOUTH);
+         new Wall (Ui, 9, 3,Direction.SOUTH);
+         new Wall (Ui, 9, 4,Direction.SOUTH);
+         new Wall (Ui, 9, 5,Direction.SOUTH);
+         new Wall (Ui, 9, 5,Direction.EAST);
+         new Wall (Ui, 8, 5,Direction.EAST);
+         new Wall (Ui, 8, 5,Direction.NORTH);
+         new Wall (Ui, 8, 4,Direction.NORTH);
+         new Wall (Ui, 8, 3,Direction.NORTH);
+         new Wall (Ui, 8, 2,Direction.NORTH);
+         new Wall (Ui, 7, 1,Direction.EAST);
+         new Wall (Ui, 6, 1,Direction.EAST);
+         new Wall (Ui, 5, 2,Direction.SOUTH);
+         new Wall (Ui, 5, 3,Direction.SOUTH);
+         new Wall (Ui, 5, 4,Direction.SOUTH);
+         new Wall (Ui, 5, 5,Direction.SOUTH);
+         new Wall (Ui, 5, 5,Direction.EAST);
+         new Wall (Ui, 4, 5,Direction.EAST);
+         new Wall (Ui, 4, 5,Direction.NORTH);
+         new Wall (Ui, 4, 4,Direction.NORTH);
+         new Wall (Ui, 4, 3,Direction.NORTH);
+         new Wall (Ui, 4, 2,Direction.NORTH);
+         new Wall (Ui, 3, 1,Direction.EAST);
+         new Wall (Ui, 2, 1,Direction.EAST);
+         new Wall (Ui, 1, 1,Direction.EAST);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         new Thing(Ui, 2, 1);
+         
+         
+       
+         
+         
+        
+                 
+         
+    }
+}
