5d2c1cc18a7de81794421f297a99f5720cb72c74
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2016-09-28 10:15:00 -0400

    update on assign.2

--- src/a2q5.java
+++ src/a2q5.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 import java.awt.Color;
 
@@ -36,6 +37,7 @@ public class a2q5 {
             new Wall(mtl,6,0,Direction.WEST);
             new Wall(mtl,7,0,Direction.WEST);
             new Wall(mtl,8,0,Direction.WEST);
+            new Wall(mtl,9,0,Direction.WEST);
             //make ten walls going down avenue,starting east 2+ walls of previous
             //turns left sees driveway... cleans it out by going down and back dowwn and back
             new Wall(mtl,0,1,Direction.EAST);
@@ -47,7 +49,87 @@ public class a2q5 {
             new Wall(mtl,6,1,Direction.EAST);
             new Wall(mtl,7,1,Direction.EAST);
             new Wall(mtl,8,1,Direction.EAST);
+            new Wall(mtl,9,1,Direction.EAST);
             //moreee
             new Wall(mtl,0,2,Direction.EAST);
+            new Wall(mtl,3,2,Direction.EAST);
+            new Wall(mtl,5,2,Direction.EAST);
+            new Wall(mtl,6,2,Direction.EAST);
+            new Wall(mtl,9,2,Direction.EAST);
+            //driveway 1
+            new Wall(mtl,1,3,Direction.NORTH);
+            new Wall(mtl,1,4,Direction.NORTH);
+            new Wall(mtl,1,5,Direction.NORTH);
+            new Wall(mtl,1,6,Direction.NORTH);
+            //
+            new Wall(mtl,1,6,Direction.EAST);
+            new Wall(mtl,2,6,Direction.EAST);
+            //
+            new Wall(mtl,2,3,Direction.SOUTH);
+            new Wall(mtl,2,4,Direction.SOUTH);
+            new Wall(mtl,2,5,Direction.SOUTH);
+            new Wall(mtl,2,6,Direction.SOUTH);
+            //drivway 2
+            new Wall(mtl,4,3,Direction.NORTH);
+            new Wall(mtl,4,4,Direction.NORTH);
+            new Wall(mtl,4,5,Direction.NORTH);
+            //
+            new Wall(mtl,4,5,Direction.EAST);
+            //
+            new Wall(mtl,4,3,Direction.SOUTH);
+            new Wall(mtl,4,4,Direction.SOUTH);
+            new Wall(mtl,4,5,Direction.SOUTH);
+            //driveway 3
+            new Wall(mtl,7,3,Direction.NORTH);
+            new Wall(mtl,7,4,Direction.NORTH);
+            new Wall(mtl,7,5,Direction.NORTH);
+            new Wall(mtl,7,6,Direction.NORTH);
+            new Wall(mtl,7,7,Direction.NORTH);
+            //
+            new Wall(mtl,8,3,Direction.SOUTH);
+            new Wall(mtl,8,4,Direction.SOUTH);
+            new Wall(mtl,8,5,Direction.SOUTH);
+            new Wall(mtl,8,6,Direction.SOUTH);
+            new Wall(mtl,8,7,Direction.SOUTH);
+            //
+            new Wall(mtl,7,7,Direction.EAST);
+            new Wall(mtl,8,7,Direction.EAST);
+            //Place things
+            //driveway 1
+            new Thing(mtl,1,2);
+            new Thing(mtl,1,3);
+            new Thing(mtl,1,4);
+            new Thing(mtl,1,5);
+            new Thing(mtl,2,2);
+            new Thing(mtl,2,4);
+            new Thing(mtl,2,6);
+            //driveway 2
+            new Thing(mtl,4,2);
+            new Thing(mtl,4,3);
+            new Thing(mtl,4,4);
+            //driveway 3
+            new Thing(mtl,7,2);
+            new Thing(mtl,7,3);
+            new Thing(mtl,7,7);
+            new Thing(mtl,8,3);
+            new Thing(mtl,8,4);
+            new Thing(mtl,8,6);
+            //while loop
+            while(!candice.frontIsClear())
+            {
+                candice.turnLeft();
+            }
+                if(candice.frontIsClear())
+                {
+                    //clean the driveway pickthing?move?
+                    candice.move();
+                }
+            
+            
+            
+            
+            
+            
+            
     }
 }
