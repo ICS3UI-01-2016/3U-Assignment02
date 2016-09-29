e99e565af8f805d72c8e3ace0e7bdcd374e7e92d
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-09-29 10:13:04 -0400

    9/29. Q3 complete. Working on Q4.

--- src/Q4.java
+++ src/Q4.java
@@ -9,7 +9,6 @@ import java.awt.Color;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author schum0689
@@ -20,39 +19,44 @@ public class Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
         City q4 = new City();
-        
-        new Wall(q4,1,1,Direction.NORTH);
-        new Wall(q4,1,1,Direction.EAST);
-        new Wall(q4,1,1,Direction.SOUTH);
-        new Wall(q4,1,1,Direction.WEST);
-        new Wall(q4,1,4,Direction.NORTH);
-        new Wall(q4,1,4,Direction.NORTH);
-        new Wall(q4,1,4,Direction.EAST);
-        new Wall(q4,1,4,Direction.SOUTH);
-        new Wall(q4,1,4,Direction.WEST);
-        new Wall(q4,4,1,Direction.NORTH);
-        new Wall(q4,4,1,Direction.EAST);
-        new Wall(q4,4,1,Direction.SOUTH);
-        new Wall(q4,4,1,Direction.WEST);
-        new Wall(q4,4,4,Direction.NORTH);
-        new Wall(q4,4,4,Direction.EAST);
-        new Wall(q4,4,4,Direction.SOUTH);
-        new Wall(q4,4,4,Direction.WEST);
-        new Wall(q4,2,2,Direction.NORTH);
-        new Wall(q4,2,3,Direction.NORTH);
-        new Wall(q4,2,2,Direction.WEST);
-        new Wall(q4,3,2,Direction.WEST);
-        new Wall(q4,3,2,Direction.SOUTH);
-        new Wall(q4,3,3,Direction.SOUTH);
-        new Wall(q4,3,3,Direction.EAST);
-        new Wall(q4,2,3,Direction.EAST);
-        
-        Robot rick = new Robot(q4,0,0,Direction.EAST);
+
+        new Wall(q4, 1, 1, Direction.NORTH);
+        new Wall(q4, 1, 1, Direction.EAST);
+        new Wall(q4, 1, 1, Direction.SOUTH);
+        new Wall(q4, 1, 1, Direction.WEST);
+        new Wall(q4, 1, 4, Direction.NORTH);
+        new Wall(q4, 1, 4, Direction.NORTH);
+        new Wall(q4, 1, 4, Direction.EAST);
+        new Wall(q4, 1, 4, Direction.SOUTH);
+        new Wall(q4, 1, 4, Direction.WEST);
+        new Wall(q4, 4, 1, Direction.NORTH);
+        new Wall(q4, 4, 1, Direction.EAST);
+        new Wall(q4, 4, 1, Direction.SOUTH);
+        new Wall(q4, 4, 1, Direction.WEST);
+        new Wall(q4, 4, 4, Direction.NORTH);
+        new Wall(q4, 4, 4, Direction.EAST);
+        new Wall(q4, 4, 4, Direction.SOUTH);
+        new Wall(q4, 4, 4, Direction.WEST);
+        new Wall(q4, 2, 2, Direction.NORTH);
+        new Wall(q4, 2, 3, Direction.NORTH);
+        new Wall(q4, 2, 2, Direction.WEST);
+        new Wall(q4, 3, 2, Direction.WEST);
+        new Wall(q4, 3, 2, Direction.SOUTH);
+        new Wall(q4, 3, 3, Direction.SOUTH);
+        new Wall(q4, 3, 3, Direction.EAST);
+        new Wall(q4, 2, 3, Direction.EAST);
+
+        Robot rick = new Robot(q4, 0, 0, Direction.EAST);
         rick.setColor(Color.black);
-        
-        
-        
+
+        if(rick.getAvenue() == 0){
+            if(rick.getStreet() == 0 || rick.getStreet() == 3){
+                rick.turnRight();
+            }
+        }
+
     }
 }
+
