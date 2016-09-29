33571c3c578b633412c00f239a9abf1acc6612c1
Val <Val@192.168.0.10>
2016-09-28 22:10:39 -0400

    A2Q5 The building blocks

--- src/A2Q5.java
+++ src/A2Q5.java
@@ -22,12 +22,12 @@ public class A2Q5 {
     public static void main(String[] args) {
         // Create a city for robot
         City Ui = new City();
-        Robot Bob = new Robot (Ui, 1, 1, Direction.SOUTH);  
+        Robot Bob = new Robot (Ui, 3, 1, Direction.SOUTH);  
         
         //Create Walls
         
-         new Wall (Ui, 1, 1,Direction.WEST);
-         new Wall (Ui, 2, 1,Direction.WEST);
+         new Wall (Ui, 11, 6,Direction.SOUTH);
+         new Wall (Ui, 10, 6,Direction.NORTH);
          new Wall (Ui, 3, 1,Direction.WEST);
          new Wall (Ui, 4, 1,Direction.WEST);
          new Wall (Ui, 5, 1,Direction.WEST);
@@ -35,20 +35,10 @@ public class A2Q5 {
          new Wall (Ui, 7, 1,Direction.WEST);
          new Wall (Ui, 8, 1,Direction.WEST);
          new Wall (Ui, 9, 1,Direction.WEST);
-         new Wall (Ui, 10, 1,Direction.WEST);
-         new Wall (Ui, 10, 1,Direction.SOUTH);
-         new Wall (Ui, 10, 1,Direction.EAST);
-         new Wall (Ui, 9, 2,Direction.SOUTH);
-         new Wall (Ui, 9, 3,Direction.SOUTH);
-         new Wall (Ui, 9, 4,Direction.SOUTH);
-         new Wall (Ui, 9, 5,Direction.SOUTH);
-         new Wall (Ui, 9, 5,Direction.EAST);
-         new Wall (Ui, 8, 5,Direction.EAST);
-         new Wall (Ui, 8, 5,Direction.NORTH);
-         new Wall (Ui, 8, 4,Direction.NORTH);
-         new Wall (Ui, 8, 3,Direction.NORTH);
-         new Wall (Ui, 8, 2,Direction.NORTH);
-         new Wall (Ui, 7, 1,Direction.EAST);
+         new Wall (Ui, 7, 4,Direction.EAST);
+         new Wall (Ui, 7, 4,Direction.SOUTH);
+         new Wall (Ui, 7, 3,Direction.SOUTH);
+         new Wall (Ui, 7, 2,Direction.NORTH);
          new Wall (Ui, 6, 1,Direction.EAST);
          new Wall (Ui, 5, 2,Direction.SOUTH);
          new Wall (Ui, 5, 3,Direction.SOUTH);
@@ -61,31 +51,67 @@ public class A2Q5 {
          new Wall (Ui, 4, 3,Direction.NORTH);
          new Wall (Ui, 4, 2,Direction.NORTH);
          new Wall (Ui, 3, 1,Direction.EAST);
-         new Wall (Ui, 2, 1,Direction.EAST);
-         new Wall (Ui, 1, 1,Direction.EAST);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
-         new Thing(Ui, 2, 1);
+         new Wall (Ui, 7, 2,Direction.SOUTH);
+         new Wall (Ui, 8, 1,Direction.EAST);
+         new Wall (Ui, 9, 1,Direction.EAST);
+         new Wall (Ui, 10, 1,Direction.WEST);
+         new Wall (Ui, 11, 1,Direction.WEST);
+         new Wall (Ui, 12, 1,Direction.WEST);
+         new Wall (Ui, 12, 1,Direction.SOUTH);
+         new Wall (Ui, 12, 1,Direction.EAST);
+         new Wall (Ui, 11, 2,Direction.SOUTH);
+         new Wall (Ui, 11, 3,Direction.SOUTH);
+         new Wall (Ui, 11, 4,Direction.SOUTH);
+         new Wall (Ui, 11, 5,Direction.SOUTH);
+         new Wall (Ui, 11, 6,Direction.EAST);
+         new Wall (Ui, 10, 6,Direction.EAST);
+         new Wall (Ui, 10, 4,Direction.NORTH);
+         new Wall (Ui, 10, 3,Direction.NORTH);
+         new Wall (Ui, 10, 2,Direction.NORTH);
+         new Wall (Ui, 10, 5,Direction.NORTH);
+         new Wall (Ui, 7, 2,Direction.NORTH);
+         new Wall (Ui, 7, 3,Direction.NORTH);
+         new Wall (Ui, 7, 4,Direction.NORTH);
+         new Thing(Ui, 4, 1);
+         new Thing(Ui, 4, 2);
+         new Thing(Ui, 4, 3);
+         new Thing(Ui, 4, 4);
+         new Thing(Ui, 5, 1);
+         new Thing(Ui, 5, 3);
+         new Thing(Ui, 5, 5);
+         new Thing(Ui, 7, 2);
+         new Thing(Ui, 7, 3);
+         new Thing(Ui, 7, 1);
+         new Thing(Ui, 10, 1);
+         new Thing(Ui, 10, 2);
+         new Thing(Ui, 10, 5);
+         new Thing(Ui, 11, 2);
+         new Thing(Ui, 11, 3);
+         new Thing(Ui, 11, 5);
+    }
+}
          
          
+         
+        
+         
+         
+    
+
+
+            
+         
+         
+                
+                
+    
+
+
        
          
          
         
                  
          
-    }
-}
+    
+
