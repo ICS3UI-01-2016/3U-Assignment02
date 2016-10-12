c72c65f4a7e6558dd8813ddf8a14ed3b13f02fe6
DhanaAnvic <fabed2976@googleapps.wrdsb.ca>
2016-10-11 22:06:46 -0400

    Create A2WrittenResponse.java

---
+++ src/A2WrittenResponse.java
@@ -0,0 +1,16 @@
+1.
+    a.) The Robot will be going to avenue 0. The Robot position will be 0,5.
+    b.) The Robot will be going to avenue 5. The Robot Position will be 5,5.
+    c.) The Robot will be going to street that is not equal to 1.
+    d.) The Robot will be not going to street that equals to 1.
+    e.) The Robot will ask if there is a wall in front of it.
+    f.) The Robot will ask if there is not a wall in front of it.
+    g.) This statement is false
+    h.) This statement is true
+
+2. 
+    Both if-else statements are not behaving differently because statement one 
+    asks if Karel can pick thing then turn right but the statement 2 asks if Karel can't pick
+    thing then turn left. For the 1st statement if the Karel can pick thing then
+    turn right does not work then he needs to turn left while statement 2 asks if
+    Karel can't pick thing then turn left does not work he needs to turn right.
